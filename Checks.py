4e2b012683ac7360792e5b5c4627294349dbb68a
Ricky842 <49582554+Ricky842@users.noreply.github.com>
2021-06-03 16:43:27 +0300

    strengthened the checks for size and type

--- Checks.py
+++ Checks.py
@@ -1,17 +1,16 @@
 pizza_name = ['BBQ Steak', 'Chicken Haiwaian',
               'Chicken Macon BBQ', 'Veg Feast']
 size_pizza = ["Large", "Small", "Medium"]
+topping_pizza = ['Pepporoni', 'Cheese', 'Beef', 'Chicken']
 
 def type_check(pizza_type):
-    if pizza_type in pizza_type:
-        print("Your choice of {} has been selected". format(pizza_type))
-    else:
-        print("Your selection is not on the list")
+    while pizza_type not in pizza_name:
+        pizza_type = input("Please choose the appropriate type from the list: ")
+    print("Your choice of {} has been selected". format(pizza_type))
     return pizza_type
 
 def size_check(pizza_size):
-    if pizza_size in size_pizza:
-        print("Your choice of {} has been selected".format(pizza_size))
-    else:
-        print("Your size selection is not available")
+    while pizza_size not in size_pizza:
+        pizza_size = input("Please choose the appropriate size from the list above: ")
+    print("Your choice of {} has been selected".format(pizza_size))
     return pizza_size
--- Pizza.py
+++ Pizza.py
@@ -7,24 +7,23 @@
 from Questions import Questions
 from Checks import type_check, size_check
 
-type_prompt = [
-    "What type of Pizza do you want?\n1.BBQ Steak\n2.Chicken Haiwaian\n3.Chicken Macon BBQ\n4.Veg Feast\n",
-]
+type_prompt = ["What type of Pizza do you want?\n1.BBQ Steak\n2.Chicken Haiwaian\n"
+"3.Chicken Macon BBQ\n4.Veg Feast\n",]
 
 size_prompt = ["What Size of Pizza do you want?\n1.Large\n2.Medium\n3.Small\n"]
 
-type_questions = [
-    Questions(type_prompt[0], "")
-]
+topping_prompt = ["Would you like extra toppings on your Pizza?"]
 
-size_questions = [
-    Questions(size_prompt[0], "")
-]
+type_questions = [Questions(type_prompt[0], "")]
+
+size_questions = [Questions(size_prompt[0], "")]
+
+topping_questions = [Questions(topping_prompt[0],"")]
 
 def pizza_check(type_questions, size_questions):
     for question in type_questions:
-        type_answer = input(question.prompt)
-        type_answer = type_check(type_answer)
+        type_answer_res = input(question.prompt)
+        type_answer = type_check(type_answer_res)
     for question in size_questions:
         size_answer = input(question.prompt)
         size_answer = size_check(size_answer)
