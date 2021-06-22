db1e32c463609bcf66be44bf7562dbc29f677782
Ricky842 <49582554+Ricky842@users.noreply.github.com>
2021-06-22 14:57:57 +0300

    cleaned prices

--- Prices.py
+++ Prices.py
@@ -6,11 +6,8 @@ topping = ["Pepporoni", "Cheese", "Beef", "Chicken"]
 toppings_price = {"Pepporoni":75, "Cheese":65, "Beef":55, "Chicken":45}
 
 def price(pizza_type, pizza_size, pizza_topping):
-    if pizza_type in type:
-        type_price = type_prices[pizza_type]
-        if pizza_size in size:
-            size_price = sizes_price[pizza_size]
-            if pizza_topping in topping:
-                topping_price = toppings_price[pizza_topping]
-        total_price = type_price + size_price + topping_price
+    type_price = type_prices[pizza_type]
+    size_price = sizes_price[pizza_size]
+    topping_price = toppings_price[pizza_topping]
+    total_price = type_price + size_price + topping_price
     return total_price
